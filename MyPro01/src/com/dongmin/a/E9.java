2b15265d74a658fb44edee6e03af4bf4d6e72754
adfree2 <lidongmin_home@126.com>
2020-09-27 14:50:41 +0800

    修改

--- MyPro01/src/com/dongmin/a/E9.java
+++ MyPro01/src/com/dongmin/a/E9.java
@@ -4,6 +4,11 @@ class MyException1 extends Exception{}
 class MyException2 extends Exception{}
 class MyException3 extends Exception{}
 
+/**
+ * 
+ * @author Administrator
+ *
+ */
 public class E9 {
 	public static void f() throws MyException1 {
 		throw new MyException1();
